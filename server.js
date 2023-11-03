8bc1ab8ec11655815118d1c5ecb6c9cfa2a95fdc
Sahvasharmid <sahvasharmid@gmail.com>
2023-11-03 19:16:45 +0400

    final changes

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use("*",function(req,res){
   })
 
 const PORT = process.env.PORT||3000;
+connectDB().then(() => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+})
 });
